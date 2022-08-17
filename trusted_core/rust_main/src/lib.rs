cce61fd08e5ab8fa268ff361b000c56a25b61071
linhan li <2694907583@qq.com>
2022-08-17 09:54:12 +0000

    Pagetable:modified
    Remove the address space,vma abstraction.
    Preserve the page table abstraction.
    Main External Interface:page_map and page_unmap.
    
    Signed-off-by: linhan li <269490783@qq.com>

--- trusted_core/rust_main/src/lib.rs
+++ trusted_core/rust_main/src/lib.rs
@@ -3,10 +3,13 @@
 #![feature(alloc_error_handler)]
 #[warn(dead_code)]
 use core::arch::asm;
+use crate::pagetable::*;
+use crate::kmem::Kmem;
 mod heapallocator_buddy;
 mod mutex;
 extern crate alloc;
 const UART_BASE: usize = 0x1000_0000;
+const KERNEL_BASE: usize = 0x80000000;
 // Macros for print
 #[macro_export]
 macro_rules! print
@@ -57,6 +60,39 @@ extern "C" fn abort() -> ! {
     }
 }
 
+extern "C" {
+    fn etext();
+    fn sheap();
+    fn end();
+}
+
+fn vspace_init(mem:&mut Kmem) -> PageTable {
+    println!(".text [{:#x}, {:#x})", KERNEL_BASE, etext as usize);
+    println!(".data [{:#x}, {:#x})", etext as usize, sheap as usize);
+    println!("heap  [{:#x}, {:#x})", sheap as usize, end as usize);
+
+    let mut pagetable_kernel = PageTable::new(mem);
+    let text_start:VirtAddr = KERNEL_BASE.into();
+    let text_start_align:VirtPageNum = text_start.align_down();
+
+    let text_end:VirtAddr = (etext as usize).into();
+    let text_end_align:VirtPageNum = text_end.align_up();
+
+    let data_start:VirtPageNum = text_end_align;
+    let heap_end:VirtAddr = (end as usize).into();
+    let heap_end_align:VirtPageNum = heap_end.align_up();
+
+    println!("mapping .text section");
+    for vpn in text_start_align.0..text_end_align.0 {
+        pagetable_kernel.page_map(vpn, vpn, PTEFlags::R | PTEFlags::X, mem);
+    }
+    println!("mapping kernel data and the heap");
+    for vpn in data_start.0..heap_end_align.0 {
+        pagetable_kernel.page_map(vpn, vpn, PTEFlags::R | PTEFlags::W, mem);
+    }
+    pagetable_kernel
+}
+
 pub mod cpu;
 pub mod ecall;
 pub mod timer;
@@ -71,8 +107,8 @@ pub mod globalallocator_impl;
 extern "C" fn rust_main() {
     let mut my_uart = uart::Uart::new(UART_BASE);
     my_uart.init();
-    let mut mem = kmem::Kmem::new();
-    let memory_set = pagetable::MemorySet::map_kernel(&mut mem);
+    let mut mem = Kmem::new();
+    let _pagetable_kernel = vspace_init(&mut mem);
     globalallocator_impl::init_heap();
     cpu::w_sstatus(cpu::r_sstatus() | cpu::SSTATUS_SIE);
     timer::clint_init();
