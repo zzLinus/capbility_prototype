ab74c05731b920df8ffa56323e8a82b01760db55
zekun xu <1060447830@qq.com>
2022-08-30 01:28:22 +0000

    GlobalAllocator: base on the buddy and slab subsystem
    An implemetation of GlobalAllocator based on
    the buddy subsystem and slab subsystem.
    
    Signed-off-by: Zekun Xu <1060447830@qq.com>

--- trusted_core/rust_main/src/globalallocator_impl.rs
+++ trusted_core/rust_main/src/globalallocator_impl.rs
@@ -1,32 +1,106 @@
 use crate::physmemallocator_buddy::{PhysMemory, BuddyAllocator};
+use crate::physmemallocator_slab::{Allocator, SlabPool, AllocationError};
 use crate::mutex::Mutex;
 use core::alloc::{GlobalAlloc, Layout};
-
-#[global_allocator]
-static ALLOCATOR: Mutex<BuddyAllocator> = Mutex::new(BuddyAllocator::new());
+use core::mem::transmute;
+use core::ptr::NonNull;
 
 const HEAP_BASE: usize = 0x8005_1520;
 const HEAP_SIZE: usize = 0x400_0000;
+const BASE_PAGE_SIZE: usize = 4096;
+const MAX_ALLOC_SIZE: usize = 1 << 17;
+
+#[allow(dead_code)]
+pub struct PhysMemAllocator {
+    slaballocator: Mutex<SlabPool<'static>>,
+    buddyallocator: Mutex<BuddyAllocator>,
+}
+
+impl PhysMemAllocator {
+    pub const fn new() -> PhysMemAllocator {
+        Self {
+            slaballocator: Mutex::new(SlabPool::new()),
+            buddyallocator: Mutex::new(BuddyAllocator::new()),
+        }
+    }
+}
 
 // Crate GlobalAllocator
-unsafe impl GlobalAlloc for Mutex<BuddyAllocator> {
+unsafe impl GlobalAlloc for PhysMemAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        self.lock().allocate(layout)
+        let mut buddyallocator = self.buddyallocator.lock();
+        match layout.size() {
+            BASE_PAGE_SIZE => {
+                let frame: *mut u8 = buddyallocator
+                    .allocate(layout);
+                frame
+            }
+            0..= MAX_ALLOC_SIZE => {
+                let mut slaballocator = self.slaballocator.lock();
+                match slaballocator.allocate(layout) {
+                    Ok(ptr) => ptr.as_ptr(),
+                    Err(AllocationError::OutOfMemory) => {
+                        // If outofmemory, slaballocator needs to request
+                        // new frames from the buddyallocator
+                        if layout.size() <= MAX_ALLOC_SIZE {
+                            let frame = buddyallocator.allocate(layout);
+                            slaballocator
+                                .refill(layout, transmute(frame as usize))
+                                .expect("Failed to refill slaballocator")
+                        } else {
+                            panic!("Larger than max_alloc_size");
+                        }
+                        slaballocator
+                            .allocate(layout)
+                            .expect("Still filed to allocate")
+                            .as_ptr()
+                    }
+                    Err(AllocationError::InvalidLayout) => {
+                        panic!("Invaild layout size")
+                    }
+                }
+            }
+        _ => {
+            let frame = buddyallocator.allocate(layout);
+            frame
+        }
+        }
     }
+
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        let block = PhysMemory{base:ptr, size:0};
-        self.lock().deallocate(block, layout);
+        let mut buddyallocator = self.buddyallocator.lock();
+        let frame = PhysMemory{base:ptr, size:0};
+        match layout.size() {
+            BASE_PAGE_SIZE => {
+                buddyallocator.deallocate(frame, layout);
+            }
+            0..= MAX_ALLOC_SIZE => {
+                if let Some(nptr) = NonNull::new(ptr) {
+                    let mut slaballocator = self.slaballocator.lock();
+                    slaballocator
+                        .deallocate(nptr, layout)
+                        .expect("Failed to deallocate (ZoneAllocator)");
+                } else {
+                }
+            }
+            _ => {
+                buddyallocator.deallocate(frame, layout);
+            }
+        }
     }
 }
 
-pub fn init_heap() {
-    unsafe {
-        let heap:PhysMemory = PhysMemory{base: HEAP_BASE as *mut u8, size: HEAP_SIZE};
-        ALLOCATOR.lock().init_region(heap);
-    }
-}
+#[global_allocator]
+pub static PHYS_MEM_ALLOCATOR: PhysMemAllocator = PhysMemAllocator::new();
 
 #[alloc_error_handler]
-pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
-    panic!("Physical memory allocation error, layout = {:?}", layout);
+pub fn handle_alloc_error(layout: Layout) -> ! {
+    panic!("Allocation error, layout = {:?}", layout);
+}
+
+
+pub fn init_mm() {
+    let physmem:PhysMemory = PhysMemory{base: HEAP_BASE as *mut u8, size: HEAP_SIZE};
+    unsafe { PHYS_MEM_ALLOCATOR.buddyallocator.lock().init_region(physmem)};
+
 }
