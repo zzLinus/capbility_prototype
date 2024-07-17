cbb8b858729ccc3d98e01cd3784d414998edfd07
Noah <noah_822@163.com>
2024-07-17 13:08:43 +0800

    use in mem atomic head

--- src/capability/alloc.rs
+++ src/capability/alloc.rs
@@ -1,3 +1,4 @@
+#![deny(clippy::perf, clippy::complexity)]
 // `KObjAllocator` is equivalent to unstable core::alloc::Allocator API
 // used internally within kernel to allow multiple strategy deployed for untyped allocation
 
@@ -5,7 +6,8 @@ use core::alloc::Layout;
 use core::mem;
 use core::ops::{Deref, DerefMut};
 use core::ptr::NonNull;
-use std::sync::{Arc, Mutex};
+use core::sync::atomic::AtomicUsize;
+use core::sync::atomic::Ordering;
 
 use super::object::UntypedObj;
 
@@ -14,32 +16,53 @@ pub enum KObjAllocErr {
     AernaOom,
     BlockOom,
 }
+
+/// allocate memory in a region bounded to an UntypedOb
+/// # Safety
+/// caller should guarantee that ptr to be deallocated is previouly allocated
+/// by invoking the same KObjAllocator
 pub(crate) unsafe trait KObjAllocator {
     fn alloc(&self, layout: Layout) -> Result<NonNull<u8>, KObjAllocErr>;
-    // SAFETY: caller should guarantee that ptr to be deallocated is previouly allocated by invoking the same KObjAllocator
     unsafe fn dealloc(&self, ptr: NonNull<u8>, layout: Layout);
 }
 
 pub struct DefaultKAllocator {
     // struct to be allocated should be fitted into this block size(in byte)
     block_size: usize,
+    // start marks block start point = UntypedObj.start + head offset
     start: usize,
     end: usize,
-    head: Arc<Mutex<usize>>,
+    head: usize,
 }
+
 impl DefaultKAllocator {
     fn bind(untyped_obj: &UntypedObj) -> Self {
         let region = untyped_obj.region;
         let (start, end) = (region.start, region.end);
         // should at least be sizeof(usize), hardcode block_size to be 64 for now
         let block_size = 64;
-        let head = Self::build_linked_free_block(start, end, block_size);
+        let head = Self::find_next_aligned(start, mem::align_of::<AtomicUsize>());
+        let start = Self::find_next_aligned(
+            head + mem::size_of::<AtomicUsize>(),
+            mem::align_of::<usize>(),
+        );
         Self {
             start,
             end,
             block_size,
-            head: Arc::new(Mutex::new(head)),
+            head,
         }
+    }
+
+    fn init_from_scratch(untyped_obj: &UntypedObj) -> Self {
+        let allocator = Self::bind(untyped_obj);
+        let first_free_block =
+            Self::build_linked_free_block(allocator.start, allocator.end, allocator.block_size);
+        unsafe {
+            // SAFETY: head is usize aligned
+            *(allocator.head as *mut _) = AtomicUsize::new(first_free_block);
+        };
+        allocator
     }
 
     // store link block meta info directly in mem instead of binding to allocator struct
@@ -62,7 +85,6 @@ impl DefaultKAllocator {
                 *(cur as *mut usize) = next_aligned;
                 cur = next_aligned;
             }
-            cur += block_size
         }
         head
     }
@@ -78,20 +100,24 @@ impl DefaultKAllocator {
     }
 }
 
+// this default allocator is designed to resolve all memory states in place
+// does not impose any other state info to be saved as part of UntypedOb
 unsafe impl KObjAllocator for DefaultKAllocator {
     fn alloc(&self, layout: Layout) -> Result<NonNull<u8>, KObjAllocErr> {
-        let mut head = self.head.lock().unwrap();
-        if *head == 0x0 {
+        // SAFETY: head is aligned so that taking ref is valid, similar op in this func follows through
+        let head = unsafe { &*(self.head as *const AtomicUsize) };
+        let first_free_block = head.load(Ordering::Acquire);
+        if first_free_block == 0x0 {
             return Err(KObjAllocErr::AernaOom);
         }
-        let end_of_block = *head + self.block_size;
-        let obj_ptr = Self::find_next_aligned(*head, layout.align());
-        if obj_ptr > end_of_block {
+        let end_of_block = first_free_block + self.block_size;
+        let obj_ptr = Self::find_next_aligned(first_free_block, layout.align());
+        if obj_ptr + layout.size() > end_of_block {
             Err(KObjAllocErr::BlockOom)
         } else {
             // SAFETY: the invariance of head: usize aligned is guaranteed during the entire (d)alloc process
-            let next_block = unsafe { *(*head as *const usize) };
-            *head = next_block;
+            let next_block = unsafe { *(first_free_block as *const usize) };
+            head.store(next_block, Ordering::Release);
             Ok(NonNull::new(obj_ptr as _).unwrap())
         }
     }
@@ -105,13 +131,13 @@ unsafe impl KObjAllocator for DefaultKAllocator {
             "Abort: ptr passed in is not managed by this allocator"
         );
         let start_of_block = Self::find_prev_aligned(ptr, mem::align_of::<usize>());
-        // reclaim this block by adding it to the free block list
-        let mut head = self.head.lock().unwrap();
+        let head = unsafe { &*(self.head as *const AtomicUsize) };
 
+        // reclaim this block by adding it to the free block list
         unsafe {
             // SAFETY: start_of_block is usize aligned
-            *(start_of_block as *mut usize) = *head;
-            *head = start_of_block;
+            *(start_of_block as *mut usize) = head.load(Ordering::Acquire);
+            head.store(start_of_block, Ordering::Release);
         }
     }
 }
@@ -125,13 +151,20 @@ impl<T, A: KObjAllocator> KObj<T, A> {
 }
 
 impl UntypedObj {
-    pub fn retype<T>(&self) -> Result<KObj<T>, KObjAllocErr>
+    pub fn retype<T>(&mut self) -> Result<KObj<T>, KObjAllocErr>
     where
         T: Default + Sized,
     {
-        let default_allocator = DefaultKAllocator::bind(self);
+        let default_allocator = if self.inited {
+            DefaultKAllocator::bind(self)
+        } else {
+            self.inited = true;
+            DefaultKAllocator::init_from_scratch(self)
+        };
         Self::retype_in::<T, DefaultKAllocator>(default_allocator)
     }
+
+    // allocator passed into should be logically binded to the upper UntypedObj type
     pub fn retype_in<T, A>(allocator: A) -> Result<KObj<T, A>, KObjAllocErr>
     where
         T: Default + Sized,
@@ -195,15 +228,25 @@ mod tests {
 
     #[test]
     fn test_alloc() {
-        let page_size = 4096;
-        let buf = vec![0u8; page_size * 4];
+        let buf = vec![0u8; 1024];
         let start = buf.as_ptr() as usize;
-        let root_untyped = UntypedObj::new(start, start + buf.len());
-        let bunch_of_kobj = (0..144)
+        let mut root_untyped = UntypedObj::new(start, start + buf.len());
+        let bunch_of_kobj = (0..4)
             .map(|_| root_untyped.retype::<PageTableObj>().unwrap())
             .collect::<Vec<_>>();
         for pagetable_kobj in &bunch_of_kobj {
-            pagetable_kobj.clear()
+            pagetable_kobj.clear();
         }
+    }
+
+    #[test]
+    fn test_mem_reclaim() {
+        let buf = vec![0u8; 1024];
+        let start = buf.as_ptr() as usize;
+        let mut root_untyped = UntypedObj::new(start, start + buf.len());
+        let kobj = root_untyped.retype::<PageTableObj>().unwrap();
+        drop(kobj);
+        let kobj = root_untyped.retype::<PageTableObj>().unwrap();
+        kobj.clear();
     }
 }
