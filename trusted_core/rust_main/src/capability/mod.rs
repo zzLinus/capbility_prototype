bde187b50d9316266729c84a83c3c7c31214f64d
shijun zhao <zhaoshijun@iie.ac.cn>
2024-07-31 12:57:55 +0000

    Revert "Merge branch 'capability-dev' into 'master'"
    This reverts merge request !8

--- trusted_core/rust_main/src/capability/mod.rs
+++
@@ -1,9 +0,0 @@
-#![allow(dead_code)]
-#![allow(non_snake_case)]
-#![allow(non_camel_case_types)]
-
-pub mod alloc;
-pub mod cap;
-pub mod cdt;
-pub mod object;
-pub mod rights;
--- trusted_core/rust_main/src/kernel_object/mod.rs
+++ trusted_core/rust_main/src/kernel_object/mod.rs
@@ -1,8 +1,6 @@
 #![allow(unused_imports)]
 pub mod endpoint;
 pub mod tcb;
-pub mod untype;
 
 pub use endpoint::Endpoint;
-pub use untype::UntypedObj;
 pub use tcb::TCB;
